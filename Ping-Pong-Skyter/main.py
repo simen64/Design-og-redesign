879235d83fbd4396db0d90454b09a9e6c41483b2
simen64 <simenmunch@gmail.com>
2024-01-30 15:23:23 +0100

    Fixed the website

--- Ping-Pong-Skyter/main.py
+++ Ping-Pong-Skyter/main.py
@@ -1,22 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
 import time
 import threading
+import random
 
 app = Flask(__name__)
 
 
 static_mode_status = False
+random_mode_status = False
 
 
 def static_mode(interval):
    global static_mode_status
    while True:
-      time.sleep(interval)
-      print("FIRE!")
       if static_mode_status == False:
+         print("turning off")
          break
       else:
          pass
+      time.sleep(interval)
+      print("FIRE!")
+      print(static_mode_status)
+
+
+def random_mode():
+   global random_mode_status
+   while True:
 
 
 
@@ -30,18 +39,22 @@ def static_mode_activation():
    global static_mode_status
    if request.method == 'POST':
 
-      interval = request.form["interval"]
-      
-      try:
-         interval = int(interval)
-      except:
-         interval = 5
+      if "stopp" in request.form:
+         print("Stopping")
+         static_mode_status = False
 
-      static_mode_status = True
-      static_mode_thread = threading.Thread(target=static_mode, args=(interval,))
-      static_mode_thread.start()
-      
+      else:
+         interval = request.form["interval"]
+         
+         try:
+            interval = int(interval)
+         except:
+            interval = 5
 
+         static_mode_status = True
+         static_mode_thread = threading.Thread(target=static_mode, args=(interval,))
+         static_mode_thread.start()
+      
    return redirect(url_for("home"))
 
 
