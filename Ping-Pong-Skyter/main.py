8da6ed31facb0bc5ba50d258582921493b0d20d9
simen64 <simenmunch@gmail.com>
2024-01-31 19:34:29 +0100

    Added stepper motor capabilities

--- Ping-Pong-Skyter/main.py
+++ Ping-Pong-Skyter/main.py
@@ -2,10 +2,69 @@ from flask import Flask, render_template, request, redirect, url_for
 import time
 import threading
 import random
+import RPi.GPIO as GPIO
 
+#Configure stuff for the stepper motor
+def load_ball():
+   in1 = 17
+   in2 = 18
+   in3 = 27
+   in4 = 22
+
+   direction = True
+
+   step_sleep = 0.002
+   step_count = 682
+
+   step_sequence = [[1,0,0,1],
+                 [1,0,0,0],
+                 [1,1,0,0],
+                 [0,1,0,0],
+                 [0,1,1,0],
+                 [0,0,1,0],
+                 [0,0,1,1],
+                 [0,0,0,1]]
+   
+   GPIO.setmode( GPIO.BCM )
+   GPIO.setup( in1, GPIO.OUT )
+   GPIO.setup( in2, GPIO.OUT )
+   GPIO.setup( in3, GPIO.OUT )
+   GPIO.setup( in4, GPIO.OUT )
+
+   GPIO.output( in1, GPIO.LOW )
+   GPIO.output( in2, GPIO.LOW )
+   GPIO.output( in3, GPIO.LOW )
+   GPIO.output( in4, GPIO.LOW )
+
+   motor_pins = [in1,in2,in3,in4]
+   motor_step_counter = 0
+
+   try:
+    i = 0
+    for i in range(step_count):
+        for pin in range(0, len(motor_pins)):
+            GPIO.output( motor_pins[pin], step_sequence[motor_step_counter][pin] )
+        if direction==True:
+            motor_step_counter = (motor_step_counter - 1) % 8
+        elif direction==False:
+            motor_step_counter = (motor_step_counter + 1) % 8
+        else: # defensive programming
+            print( "uh oh... direction should *always* be either True or False" )
+            exit( 1 )
+        time.sleep( step_sleep )
+   except:
+      pass
+
+   GPIO.output( in1, GPIO.LOW )
+   GPIO.output( in2, GPIO.LOW )
+   GPIO.output( in3, GPIO.LOW )
+   GPIO.output( in4, GPIO.LOW )
+   GPIO.cleanup()
+
+#Set up Flask
 app = Flask(__name__)
 
-
+#Set up variables
 static_mode_status = False
 random_mode_status = False
 
@@ -20,6 +79,7 @@ def static_mode(interval):
          pass
       time.sleep(interval)
       print("FIRE!")
+      load_ball()
 
 
 def random_mode():
@@ -33,6 +93,7 @@ def random_mode():
       interval = random.randint(1,7)
       time.sleep(interval)
       print("FIRE!")
+      load_ball()
 
 
 @app.route("/")
